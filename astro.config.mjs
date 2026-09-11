f658030f62c856944b263dea9135bcb3acc6cee0
miguel-jaeger <miguel.jaeger@gmail.com>
2026-09-11 12:33:50 -0500

    refactor: replace deprecated @astrojs/image and @astrojs/tailwind integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,10 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import tailwind from "@astrojs/tailwind";
-
-// https://astro.build/config
-import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://miguel-jaeger.github.io',
   base: '/mi-curriculum',
-  integrations: [mdx(), sitemap(), tailwind(), image(
-    {
-      serviceEntryPoint: '@astrojs/image/sharp',
-      cacheDir: "./.cache/image",
-      logLevel: 'debug',
-    }
-  )]
+  integrations: [mdx(), sitemap()]
 });
